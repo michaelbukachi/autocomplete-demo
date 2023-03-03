aa799cd6d7c151d4f56459c65ff07e3ddfb1621a
Michael Bukachi <michaelbukachi@gmail.com>
2023-03-03 14:04:33 +0300

    refactor: Improvements

--- main.py
+++ main.py
@@ -5,32 +5,32 @@ from starlette.responses import HTMLResponse, JSONResponse
 
 app = FastAPI()
 fake = Faker()
-redis = Redis('localhost', 6379, decode_responses=True)
+redis = Redis("localhost", 6379, decode_responses=True)
 
-NAMES = [fake.name() for i in range(500)]
+NAMES = [f"{fake.name()}-{i}" for i in range(500)]
 
 
 @app.on_event("startup")
 async def startup_event():
-    key = 'names_list'
+    key = "names_list"
     pipeline = redis.pipeline(True)
     pipeline.delete(key)
     pipeline.rpush(key, *NAMES)
     pipeline.execute()
-    print('Names loaded into Redis.')
+    print("Names loaded into Redis.")
 
 
 @app.get("/", response_class=HTMLResponse)
 async def index():
-    with open('index.html') as fp:
+    with open("index.html") as fp:
         html_content = fp.read()
     return HTMLResponse(content=html_content, status_code=200)
 
 
 @app.get("/autocomplete", response_class=JSONResponse)
-async def get_names_autocomplete_list(prefix: str = ''):
+async def get_names_autocomplete_list(prefix: str = ""):
     if prefix:
-        key = 'names_list'
+        key = "names_list"
         # We have to load the autocomplete list into memory
         # since there's no way, currently, to do a
         # 'startswith' check on a redis list
@@ -40,7 +40,8 @@ async def get_names_autocomplete_list(prefix: str = ''):
         matches = []
         for name in names:
             if name.lower().startswith(prefix):
-                matches.append(name)
+                name, pk = name.split("-")
+                matches.append({"name": name, "id": pk})
 
         return matches
 
